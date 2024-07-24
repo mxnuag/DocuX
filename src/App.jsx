e535a3194fb2de595dc7218b0f4e60f21fbc2e39
AGARWALMANU3008 <manuagarwal3008@gmail.com>
2024-07-24 23:33:57 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Background from './components/Background'
+import Fore from './components/Fore'
+
+
+function App() {
+  return (
+    <div className=' relative w-full h-screen bg-zinc-800'>
+      <Background />
+      <Fore/>
+    </div>
+
+  )
+}
+
+export default App
